e89890c477fcb2f5f007ea4b23be9a2fd787eb41
Simon Thiesen <Simon.Thiesen@online.de>
2025-09-02 21:38:56 +0200

    Update build.rs

--- src/raw/build.rs
+++ src/raw/build.rs
@@ -130,7 +130,7 @@ impl<W: io::Write> Builder<W> {
         Ok(Builder {
             wtr,
             unfinished: UnfinishedNodes::new(),
-            registry: Registry::new(30_000_000, 10),
+            registry: Registry::new(300_000, 5),
             last: None,
             last_addr: NONE_ADDRESS,
             len: 0,
